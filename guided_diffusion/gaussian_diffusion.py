076eae04c437fa4f246212524cbedb2c74a70840
anditp <anditp@gmail.com>
2024-06-01 09:17:31 +0200

    Changes

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -877,7 +877,7 @@ class GaussianDiffusion:
                 ModelMeanType.EPSILON: noise,
             }[self.model_mean_type]
             assert model_output.shape == target.shape == x_start.shape
-            scaling = th.flip(th.arange(1, 2001) ** 3, 0)
+            scaling = th.flip(th.arange(1, 2001) ** 3, [0])
             terms["mse"] = mean_flat((target - model_output) ** 2 * scaling)
             if "vb" in terms:
                 terms["loss"] = terms["mse"] + terms["vb"]
